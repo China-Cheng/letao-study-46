2cf11ec314d6fee353343684eda10aa82ec3a8fa
name-WuCheng <1014967685qq@.com>
2018-06-20 09:37:35 +0800

    实现登录功能

--- js/common.js
+++ js/common.js
@@ -7,5 +7,45 @@ $(function(){
 		$(this).find('ul').slideToggle();
 
 	});
+}
+// 全局配置
+var APP = {
+    // 接口的基础配置
+    baseUrl:'http://fullstack.net.cn:3000'
+}
 
 });
+
+// 将表单数据已JSON的格式返回
+$.fn.serializeToJson = function () {
+
+    var formAry = this.serializeArray();
+
+    var result = {};
+
+    formAry.forEach(function (item) {
+
+        result[item.name] = item.value;
+    })
+    return result;
+}
+
+// 获取地址栏中的参数
+function  getUrlParams(name) {
+    // 截取第一个之后的所有数据
+    var search = location.search.slice(1);
+    // 用&符号分割成数组
+    var ary = search.split('&');
+    // 循环这个数组
+    for (var i = 0; i < ary.length; i++) {
+        // 再用=把每一个单元分割为数组
+        var ary2 = ary[i].split('=');
+        // 如果数组中的数据等于传进来的数据
+        if (ary2[0] == name) {
+            // 那么传出去
+            return ary2[1];
+        }
+    }
+    // 否则传出-1
+    return -1;
+}   
---
+++ js/login.js
@@ -0,0 +1,36 @@
+$(function () {
+    // 为登录按钮添加点击事件
+    // 获取用户输入的用户名和密码
+    // 对用户输入的信息格式进行校验
+    // 如果校验成功，调用接口实现登录
+    // 
+    // 当登录按钮被点击的时候
+    $('#loginBtn').on('click',function () {
+        // 获取表单数据
+        var result = $('#loginForm').serializeToJson();
+        // 验证用户是否输入用户名
+        if (!$.trim(result.username)) {
+            alert('请输入用户名')
+            return;
+        }
+        if (!$.trim(result.password)) {
+            alert('请输入密码')
+            return;
+        }
+        // 发送登录请求
+        $.ajax({
+            type:'post',
+            url:`${APP.beseUrl}/employee/employeeLogin`,
+            data:result,
+            success:function (response) {
+                if (response.success) {
+                    // 登录成功
+                    location.href = 'user.html';
+                }else {
+                    // 登录失败
+                    alert(response.message)
+                }
+            }
+        })
+    })  
+})
