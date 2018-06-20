f5c5b9219730dff5332c7e4baac80c225ca740ab
name-WuCheng <1014967685qq@.com>
2018-06-20 09:51:47 +0800

    登录提交

--- js/common.js
+++ js/common.js
@@ -1,51 +1,59 @@
+
 $(function(){
 
-	var navLi = $('.navs li')
+    var navLi = $('.navs li')
 
-	navLi.on('click',function(){
+    navLi.on('click',function(){
 
-		$(this).find('ul').slideToggle();
+        $(this).find('ul').slideToggle();
 
-	});
-}
+    });
+
+});
+
+
 // 全局配置
 var APP = {
     // 接口的基础配置
     baseUrl:'http://fullstack.net.cn:3000'
 }
 
-});
 
-// 将表单数据已JSON的格式返回
+
+
+// 将表单数据以 json 的形式返回 (jouery扩展方法)
+
 $.fn.serializeToJson = function () {
 
-    var formAry = this.serializeArray();
+    // serializeArray() 获取表单里的数据 通过表单里的name值获取的
+    var formArray = this.serializeArray();
 
+    // result = {username:'zhangsna',password:123}
     var result = {};
 
-    formAry.forEach(function (item) {
-
+    formArray.forEach(function(item){
         result[item.name] = item.value;
-    })
+
+    });
+
     return result;
 }
 
-// 获取地址栏中的参数
-function  getUrlParams(name) {
-    // 截取第一个之后的所有数据
+
+// 封装定义获取id的方法
+function getUrlParams(name) {
+    // 通过 location.srarch 获取地址栏  slice 拷贝数组
     var search = location.search.slice(1);
-    // 用&符号分割成数组
-    var ary = search.split('&');
-    // 循环这个数组
-    for (var i = 0; i < ary.length; i++) {
-        // 再用=把每一个单元分割为数组
-        var ary2 = ary[i].split('=');
-        // 如果数组中的数据等于传进来的数据
-        if (ary2[0] == name) {
-            // 那么传出去
+    // 通过split分隔
+    var ary1 = search.split('&');
+
+    for(var i = 0; i < ary1.length; i++) {
+        var ary2 = ary1[i].split('=');
+        if(ary2[0] == name) {
             return ary2[1];
         }
     }
-    // 否则传出-1
+
     return -1;
-}   
+
+}
